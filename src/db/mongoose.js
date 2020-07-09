931ec09a5d0d82fd2140c2f31d5feea2f776e0b4
bhanujggandhi <gandhibhanuj@gmail.com>
2020-07-09 15:24:13 +0530

    Keys config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
+const { mongoUri } = require("../config/keys");
 
-mongoose.connect("mongodb://127.0.0.1:27017/dashboard-api", {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(mongoUri, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((e) => {
+    console.log("Database Error ", e);
+  });
